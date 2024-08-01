345e4bdf1b6f753bca0ffeeedec89f9e2ff53500
Alvaro Felipe Garcia Mendez <alvarofelipe12@gmail.com>
2024-08-01 11:38:00 -0400

    Define an HTML Class in JSX

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="myDiv">
       {/* here's a comment */}
       <h1>This is a block of JSX</h1>
       <p>Here's a subtitle</p>
