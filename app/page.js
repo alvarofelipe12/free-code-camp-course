4b16bfc3ae70cf8490d6eb247d848e8fd57bc6d0
Alvaro Felipe Garcia Mendez <alvarofelipe12@gmail.com>
2024-10-23 14:16:51 -0400

    US 1 to 4

--- app/page.js
+++ app/page.js
@@ -1,7 +1,25 @@
 export default function Home() {
+  let breakLength = 5;
+  let sessionLength = 25;
   return (
-    <div className="header">
+    <div className="grid">
       {/* here's a comment */}
+      <div className="container">
+        <h3 className="" id="break-label">Break Length</h3>
+        <div className="sub-container">
+          <button type="button" className="" id="break-decrement">⬇</button>
+          <h3 className="">{breakLength}</h3>
+          <button type="button" className="" id="break-increment">⬆</button>
+        </div>
+      </div>
+      <div className="container">
+        <h3 className="" id="session-label">Session Length</h3>
+        <div className="sub-container">
+          <button type="button" className="" id="session-decrement">⬇</button>
+          <h3 className="">{sessionLength}</h3>
+          <button type="button" className="" id="session-increment">⬆</button>
+        </div>
+      </div>
     </div>
   );
 }
